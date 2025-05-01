7abbe654ebd9c839a84e74c83995f289f7c70c0d
akhileshchoudary <akhileshchoudary1994@gmail.com>
2025-04-30 20:39:33 -0500

    test 2

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ templates = Jinja2Templates(directory="templates")
 # Define input data model
 class InsuranceInput(BaseModel):
     age: int
-    # sex: str
+    sex: str
     bmi: float
     children: int
     smoker: str
-    #region: str
+    region: str
 
 # Home route
 @app.get("/")
@@ -31,11 +31,11 @@ def home(request: Request):
 def predict_charges(input_data: InsuranceInput):
     new_data = pd.DataFrame({
         "age": [input_data.age],
-        #"sex": [input_data.sex],
+        "sex": [input_data.sex],
         "bmi": [input_data.bmi],
         "children": [input_data.children],
         "smoker": [input_data.smoker],
-        #"region": [input_data.region]
+        "region": [input_data.region]
     })
     new_data["smoker"] = new_data["smoker"].map({"yes": 1, "no": 0})
     new_data = new_data[["age", "bmi", "children", "smoker"]]  # Use only important features
